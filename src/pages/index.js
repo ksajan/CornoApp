e21b4e93a38bae45e89e459fe8789d6e9d558aa8
Sajan <ksajan12@gmail.com>
2020-04-03 08:43:45 -0700

    fixxxxxxxxxeeeeeeddddd

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,6 +34,28 @@ const IndexPage = () => {
     }
 
     const { data = [] } = response;
+
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if (!hasData) return;
+
+    const geoGjson = {
+      type: "FeatureCollection",
+      features: data.map((country = {}) => {
+      const { countryInfo = {}} = country;
+      const { lat, long, lng } = countryInfo;
+      return {
+        type: "Feature",
+        properties: {
+          ...country,
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [lng, lat]
+        }
+      }
+    })
+    }
   }
 
   const mapSettings = {
